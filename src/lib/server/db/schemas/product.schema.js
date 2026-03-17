617babb16df9718b865aec7e5bcd867c72e6cbfe
ashcroft08 <johangracia40@gmail.com>
2026-03-16 22:12:00 -0500

    Corrección del esquema de productos

--- src/lib/server/db/schemas/product.schema.js
+++ src/lib/server/db/schemas/product.schema.js
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { pgTable, text, timestamp, boolean, index } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, decimal, integer } from "drizzle-orm/pg-core";
 
 // Tabla: Materiales
 export const materiales = pgTable('materiales', {
